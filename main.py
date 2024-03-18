46044df89d7b9cc2120c4bedbdfab270d0b558a6
Weitian <31085999+weitian8@users.noreply.github.com>
2024-03-18 14:27:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,17 @@ def get_location(ip):
     return None
 
 def convert_ips(input_urls, output_files):
-
     for input_url, output_file in zip(input_urls, output_files):
         ips = get_ips_from_url(input_url)  # 获取URL中的IP地址列表
         
         with open(output_file, 'w') as f:
-            js = 1
             for ip in ips:
                 location = get_location(ip)
                 if location:
-                    f.write(f"{ip}#{location} {js}\n")
+                    f.write(f"{ip}#{location}\n")
                 else:
-                    f.write(f"{ip}#Unknown {js}\n")
-js = js + 1
+                    f.write(f"{ip}#Unknown\n")
+
 if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf"]  # 包含IP地址的txt文件的多个URL
     output_files = ["bestproxy.txt", "bestcf.txt"]
